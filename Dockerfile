39e34cb7fb318acb68a498a93d83896af87e2806
Gowri <gowri.sankar@aligent.com.au>
2023-01-13 14:08:34 +1030

    :sparkles: feat: init pipe

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM php:8.1-alpine
+
+RUN apk add mariadb-client procps --no-cache
+
+COPY --from=mlocati/php-extension-installer /usr/bin/install-php-extensions /usr/local/bin/
+
+RUN install-php-extensions gd bcmath zip intl xsl pdo_mysql soap sockets @composer
+
+ENV COMPOSER_ALLOW_SUPERUSER 1
+
+RUN echo "memory_limit = -1" > $PHP_INI_DIR/conf.d/php-memory-limits.ini
+
+COPY pipe.sh /
+
+ENTRYPOINT ["/pipe.sh"]
---
+++ pipe.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env ash
+
+set -e
+
+composer --version
+composer create-project --repository-url="https://mirror.mage-os.org/" "magento/project-community-edition:>=2.4.5 <2.4.6" ./magento2 --no-install
+cd magento2
+composer config --no-interaction allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
+composer config --no-interaction allow-plugins.laminas/laminas-dependency-plugin true
+composer config --no-interaction allow-plugins.magento/* true
+cat composer.json
+composer install
+cd dev/tests/integration
+sed -i "s/'db-host' => 'localhost'/'db-host' => '127.0.0.1'/" etc/install-config-mysql.php.dist
+sed -i "s/'db-user' => 'root'/'db-user' => 'user'/" etc/install-config-mysql.php.dist
+sed -i "s/'db-password' => '123123q'/'db-password' => 'password'/" etc/install-config-mysql.php.dist
+sed -i "s/'elasticsearch-host' => 'localhost'/'elasticsearch-host' => '127.0.0.1'/" etc/install-config-mysql.php.dist
+sed -i "s/'amqp-host' => 'localhost'/'amqp-host' => '127.0.0.1'/" etc/install-config-mysql.php.dist
+php ../../../vendor/bin/phpunit ../../../vendor/magento/magento2-base/dev/tests/integration/testsuite/Magento/Framework/MessageQueue/TopologyTest.php
