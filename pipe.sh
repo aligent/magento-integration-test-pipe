3ece92c5b45ea8cc85db6bd426f78020289c3ce8
Gowri <gowri.sankar@aligent.com.au>
2023-01-13 19:49:16 +1030

    :sparkles: feat: add graphql tests

--- pipe.sh
+++ pipe.sh
@@ -51,7 +51,33 @@ run_rest_api_tests () {
 }
 
 run_graphql_api_tests () {
-  echo "Running graphql api tests"
+  mysql -h host.docker.internal -uroot -prootpassword -e "show databases;CREATE DATABASE IF NOT EXISTS magento_graphql_tests;GRANT ALL ON magento_graphql_tests.* TO 'user'@'%';FLUSH PRIVILEGES;"
+  composer create-project --repository-url="https://mirror.mage-os.org/" "magento/project-community-edition:>=2.4.5 <2.4.6" ./magento2 --no-install
+  cd magento2
+  composer config --no-interaction allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
+  composer config --no-interaction allow-plugins.laminas/laminas-dependency-plugin true
+  composer config --no-interaction allow-plugins.magento/* true
+  cat composer.json
+  composer install
+  cd dev/tests/api-functional
+
+  cp phpunit_graphql.xml.dist phpunit_graphql.xml
+  cp config/install-config-mysql.php.dist config/install-config-mysql-graphql.php
+  sed -i 's/name="TESTS_MAGENTO_INSTALLATION" value="disabled"/name="TESTS_MAGENTO_INSTALLATION" value="enabled"/' phpunit_graphql.xml
+  sed -i 's#http://magento.url#http://127.0.0.1:8082/index.php/#' phpunit_graphql.xml
+  sed -i 's/value="admin"/value="Test Webservice User"/' phpunit_graphql.xml
+  sed -i 's/value="123123q"/value="Test Webservice API key"/' phpunit_graphql.xml
+
+  sed -i "s,http://localhost/,http://127.0.0.1:8082/index.php/," config/install-config-mysql-graphql.php
+  sed -i "s/'db-host'                      => 'localhost'/'db-host' => 'host.docker.internal'/" config/install-config-mysql-graphql.php
+  sed -i "s/'db-name'                      => 'magento_functional_tests'/'db-name' => 'magento_graphql_tests'/" config/install-config-mysql-graphql.php
+  sed -i "s/'db-user'                      => 'root'/'db-user' => 'user'/" config/install-config-mysql-graphql.php
+  sed -i "s/'db-password'                  => ''/'db-password' => 'password'/" config/install-config-mysql-graphql.php
+  sed -i "s/'elasticsearch-host'           => 'localhost'/'elasticsearch-host' => 'host.docker.internal'/" config/install-config-mysql-graphql.php
+  cd ../../../
+  php -S 127.0.0.1:8082 -t ./pub/ ./phpserver/router.php &
+  sleep 5
+  vendor/bin/phpunit -c $(pwd)/dev/tests/api-functional/phpunit_graphql.xml vendor/magento/magento2-base/dev/tests/api-functional/testsuite/Magento/GraphQl/Directory/CurrencyTest.php
 }
 
 case $TYPE in
